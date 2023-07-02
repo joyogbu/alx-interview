920b5569d7a8223500902232a665ef45bc88d33c
joyogbu <obyjoy@yahoo.com>
2023-07-02 18:41:25 +0000

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 # pascal triangle
-from math import factorial
+# from math import factorial
+
+
 def pascal_triangle(n):
     e_list = []
-    #mylist = []
+    # mylist = []
     if n <= 0:
         return (e_list)
     for i in range(n):
         for j in range(i+1):
             mylist = []
-            elem = (factorial(i)//(factorial(j)*factorial(i -j)))
+            elem = (factorial(i) // (factorial(j) * factorial(i - j)))
             mylist.append(elem)
-    print (mylist)
-#    print('\n')
+        return(mylist)
+    print('\n')
+
 
 print(pascal_triangle(5))
