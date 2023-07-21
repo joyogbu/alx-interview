558720cfa893e377be4122d9159e2ef12f304893
joyogbu <obyjoy@yahoo.com>
2023-07-20 18:37:13 -0700

    task 0 edited

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,36 +3,44 @@
 
 
 import sys
+import signal
 
 
-count = 0
-my_list = []
-# item_list = []
-for line in sys.stdin:
-    item_list = []
-    byte_list = []
-    byte_list2 = []
-    dictionary = {}
-    total = 0
-    my_list.append(line)
-    count = count + 1
-    # my_dict.append(line)
-    if (count == 10):
-        # my_list.append(line)
-        # print(my_list)
-        for item in my_list:
-            # dictionary = {}
-            item2 = item.split()
-            # if item2[-2] not in item_list:
-            item_list.append(item2[-2])
-            byte_list.append(item2[-1])
-            byte_list2 = map(int, byte_list)
-            total = sum(byte_list2)
-            dictionary[item2[-2]] = dictionary.get(item2[-2], 0) + 1
-        # count = 0
-        # print(item_list)
-        # print(dictionary)
-        print("File size: {}".format(total))
-        for k, v in sorted(dictionary.items()):
-            print("{}: {}".format(k, v))
-        count = 0
+try:
+    count = 0
+    my_list = []
+    # item_list = []
+
+    for line in sys.stdin:
+        item_list = []
+        byte_list = []
+        byte_list2 = []
+        dictionary = {}
+        total = 0
+        my_list.append(line)
+        count = count + 1
+        # my_dict.append(line)
+        if count == 10:
+            for item in my_list:
+                item2 = item.split()
+                item_list.append(item2[-2])
+                byte_list.append(item2[-1])
+                byte_list2 = map(int, byte_list)
+                total = sum(byte_list2)
+                dictionary[item2[-2]] = dictionary.get(item2[-2], 0) + 1
+            print("File size: {}".format(total))
+            for k, v in sorted(dictionary.items()):
+                print("{}: {}".format(k, v))
+            count = 0
+except KeyboardInterrupt:
+    for item in my_list:
+        item2 = item.split()
+        item_list.append(item2[-2])
+        byte_list.append(item2[-1])
+        byte_list2 = map(int, byte_list)
+        total = sum(byte_list2)
+        dictionary[item2[-2]] = dictionary.get(item2[-2], 0) + 1
+    print("File size: {}".format(total))
+    for k, v in sorted(dictionary.items()):
+        print("{}: {}".format(k, v))
+    count = 0
