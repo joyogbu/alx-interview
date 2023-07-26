448ab9e55bf11a38ae5a94625bd8d217e7fea8f6
joyogbu <obyjoy@yahoo.com>
2023-07-25 17:03:29 -0700

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,6 +8,8 @@ import math
 def validUTF8(data):
     '''defining the function'''
     # lis = []
+    if type(data) is not list:
+        return False
     lis2 = []
     lis3 = []
     for x in data:
