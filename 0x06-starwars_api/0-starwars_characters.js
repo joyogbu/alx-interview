de3c37f16bf50e01a0216b1dc9f095d29398e9bc
joyogbu <obyjoy@yahoo.com>
2023-08-10 16:09:15 -0700

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const arg = process.argv;
 const my_id = arg[2]
-console.log(my_id)
 const url = "https://swapi-api.alx-tools.com/api/films/" + my_id;
-console.log(url)
 const request = require('request')
 request(url, function(error, response, body) {
 	if (error)
