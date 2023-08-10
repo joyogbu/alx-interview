2d2b174e49382cf3ccc9ba8152f250f44581e783
joyogbu <obyjoy@yahoo.com>
2023-08-10 16:34:08 -0700

    task 0 edited

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const arg = process.argv;
-const my_id = arg[2]
-const url = "https://swapi-api.alx-tools.com/api/films/" + my_id;
-const request = require('request')
-request(url, function(error, response, body) {
-	if (error)
-		console.log(error)
-	else {
-		const myData = JSON.parse(body).characters;
-		myData.forEach((character) => {
-			request(character, function (error, response, body) {
-        		console.log(JSON.parse(body).name);
-			});
-		});
-	}
+const myId = arg[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + myId;
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const myData = JSON.parse(body).characters;
+    myData.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        }
+        console.log(JSON.parse(body).name);
+      });
+    });
+  }
 });
