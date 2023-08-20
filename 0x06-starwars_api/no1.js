7087d40759787445cb218468c92c8852b002f3c2
joyogbu <obyjoy@yahoo.com>
2023-08-20 10:05:30 -0700

    task 0

--- 0x06-starwars_api/no1.js
+++ 0x06-starwars_api/no1.js
@@ -3,18 +3,23 @@ const arg = process.argv;
 const myId = arg[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + myId;
 const request = require('request');
-request(url, async (error, response, body) => {
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    const myData = JSON.parse(body).characters;
+    myArray = []
+    const myData = await JSON.parse(body).characters;
     myData.forEach(async (character) => {
-      await request(character, function (error, response, body) {
+      const myArr = []
+      myArray = await request(character, async function (error, response, body) {
         if (error) {
           console.log(error);
         }
-	//});
+	/*myArr.push(JSON.parse(body).name)*/
+	await Promise.all(myArray)
         console.log(JSON.parse(body).name);
+	/*await Promise.all(myData)*/
+	/*console.log(res)*/
       });
     });
   }
