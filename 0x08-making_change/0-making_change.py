283342180d002e98f8eb9f8eaa86a164958c5b0f
joyogbu <obyjoy@yahoo.com>
2023-08-24 12:18:35 +0000

    task 0 edited

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,12 +14,10 @@ def makeChange(coins, total):
     res = [sys.maxsize] * (total + 1)
     res[0] = 0
     for i in range(1, total + 1):
-        res[i] = sys.maxsize
-    for i in range(1, total + 1):
         for j in range(length):
             if coins[j] <= i:
                 sub_res = res[i - coins[j]]
-                if (sub_res != sys.maxsize and sub_res + 1 < res[i]):
+                if (sub_res != str(sys.maxsize) and sub_res + 1 < res[i]):
                     res[i] = sub_res + 1
     if res[total] == sys.maxsize:
         return -1
